688a18a76d5155b98f5071d1aa0655594a773a12
cg2p <jezcaine@gmail.com>
2022-10-18 13:24:23 +0100

    test data read from COS

--- load-data/load-data.py
+++ load-data/load-data.py
@@ -4,4 +4,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+# -- Build the model
+# train_df: pd.DataFrame = pd.read_csv('Resources/mnist_train.csv', header=None)
+test_df: pd.DataFrame = pd.read_csv('https://s3.eu-gb.cloud-object-storage.appdomain.cloud/caine-mnist-dataset/mnist_test.csv', header=None)
+
+print(test_df.loc[1:, 1:2].values)
 print("data loaded")
--- walkthrough.md
+++ walkthrough.md
@@ -8,3 +8,6 @@ Create a Hello World python app
 - create app in CE with additional settings of Listening port 3333
 - CE generates URL to serve "Hello World!"
 
+Load data from COS
+
+https://s3.eu-gb.cloud-object-storage.appdomain.cloud/caine-mnist-dataset/mnist_test.csv
