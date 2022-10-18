c28660888da35a320a253f587b7dfd7231c11fff
cg2p <jezcaine@gmail.com>
2022-10-18 13:58:37 +0100

    upd loc model

--- load-data/load-data.py
+++ load-data/load-data.py
@@ -4,14 +4,18 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+LOC_TRAIN_CSV = 'https://s3.eu-gb.cloud-object-storage.appdomain.cloud/caine-mnist-dataset/mnist_train.csv' 
+LOC_TEST_CSV = 'https://s3.eu-gb.cloud-object-storage.appdomain.cloud/caine-mnist-dataset/mnist_test.csv'
+LOC_MODEL = 'https://s3.us-east.cloud-object-storage.appdomain.cloud/caine-mnist-model/cities.csv'
+
 # -- Build the model
-# train_df: pd.DataFrame = pd.read_csv('Resources/mnist_train.csv', header=None)
-test_df: pd.DataFrame = pd.read_csv('https://s3.eu-gb.cloud-object-storage.appdomain.cloud/caine-mnist-dataset/mnist_test.csv', header=None)
+# train_df: pd.DataFrame = pd.read_csv(LOC_TRAIN_CSV, header=None)
+test_df: pd.DataFrame = pd.read_csv(LOC_TEST_CSV, header=None)
 
 data: test_df.loc[1:, 1:2].values
 
 cities = pd.DataFrame([['Sacramento', 'California'], ['Miami', 'Florida']], columns=['City', 'State'])
-cities.to_csv('https://s3.eu-gb.cloud-object-storage.appdomain.cloud/caine-mnist-model/cities.csv')
+cities.to_csv(LOC_MODEL)
 
 print(data)
 print("data loaded")
--- walkthrough.md
+++ walkthrough.md
@@ -8,6 +8,16 @@ Create a Hello World python app
 - create app in CE with additional settings of Listening port 3333
 - CE generates URL to serve "Hello World!"
 
+## Load Model data into COS
+bucket `caine-mnist-dataset`
+give public access
+`https://s3.eu-gb.cloud-object-storage.appdomain.cloud/caine-mnist-dataset/mnist_test.csv`
+
+## Create buck for model to be written to
+create service id
+assign service id as Object Writer to bucket `caine-mnist-model`
+
+## Test Load data and write out CSC
 Load data from COS
 
-https://s3.eu-gb.cloud-object-storage.appdomain.cloud/caine-mnist-dataset/mnist_test.csv
+
