1991cf9e2ca6570659a9779e9729842f6700f061
cg2p <jezcaine@gmail.com>
2022-10-18 13:45:01 +0100

    write csv to COS

--- load-data/load-data.py
+++ load-data/load-data.py
@@ -8,5 +8,10 @@ import matplotlib.pyplot as plt
 # train_df: pd.DataFrame = pd.read_csv('Resources/mnist_train.csv', header=None)
 test_df: pd.DataFrame = pd.read_csv('https://s3.eu-gb.cloud-object-storage.appdomain.cloud/caine-mnist-dataset/mnist_test.csv', header=None)
 
-print(test_df.loc[1:, 1:2].values)
+data: test_df.loc[1:, 1:2].values
+
+cities = pd.DataFrame([['Sacramento', 'California'], ['Miami', 'Florida']], columns=['City', 'State'])
+cities.to_csv('https://s3.eu-gb.cloud-object-storage.appdomain.cloud/caine-mnist-model/cities.csv')
+
+print(data)
 print("data loaded")
