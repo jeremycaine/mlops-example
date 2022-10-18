4135e9ab25e8de6d92b2d7eea2820c403d6b7dc5
cg2p <jezcaine@gmail.com>
2022-10-18 15:14:58 +0100

    upd for service creds

--- load-data/load-data.py
+++ load-data/load-data.py
@@ -4,18 +4,28 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-LOC_TRAIN_CSV = 'https://s3.eu-gb.cloud-object-storage.appdomain.cloud/caine-mnist-dataset/mnist_train.csv' 
-LOC_TEST_CSV = 'https://s3.eu-gb.cloud-object-storage.appdomain.cloud/caine-mnist-dataset/mnist_test.csv'
-LOC_MODEL = 'https://s3.us-east.cloud-object-storage.appdomain.cloud/caine-mnist-model/cities.csv'
+PATH = os.getenv('PATH')
+print(PATH)
 
+#LOC_TRAIN_CSV = 'https://s3.eu-gb.cloud-object-storage.appdomain.cloud/caine-mnist-dataset/mnist_train.csv' 
+#LOC_TEST_CSV = 'https://s3.eu-gb.cloud-object-storage.appdomain.cloud/caine-mnist-dataset/mnist_test.csv'
+#LOC_MODEL = 'https://s3.us-east.cloud-object-storage.appdomain.cloud/caine-mnist-model/cities.csv'
+
+LOC_TRAIN_CSV = os.get('LOC_TRAIN_CSV')
+LOC_TEST_CSV = os.get('LOC_TEST_CSV')
+LOC_MODEL = os.get('LOC_MODEL')
+
+COS_IAM_KEY = os.get('CLOUD_OBJECT_STORAGE_APIKEY')
+print(COS_IAM_KEY)
 # -- Build the model
 # train_df: pd.DataFrame = pd.read_csv(LOC_TRAIN_CSV, header=None)
 test_df: pd.DataFrame = pd.read_csv(LOC_TEST_CSV, header=None)
 
 data: test_df.loc[1:, 1:2].values
+print(data)
 
 cities = pd.DataFrame([['Sacramento', 'California'], ['Miami', 'Florida']], columns=['City', 'State'])
-cities.to_csv(LOC_MODEL)
+x = cities.to_csv(index=False)
+print(x)
 
-print(data)
 print("data loaded")
